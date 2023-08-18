caae0a58bb0958bb4fff149dc261a59a20c20dcb
alisonsantosofc <alisonsantoso22@gmail.com>
2023-08-18 09:43:42 -0300

    build: create frontend structure

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'airbnb-base',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'no-shadow': 'off'
+  },
+}
