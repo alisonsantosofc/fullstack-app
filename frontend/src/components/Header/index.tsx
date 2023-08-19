8dd12f1da172575db07518b84b9f95c707531db4
alisonsantosofc <alisonsantoso22@gmail.com>
2023-08-18 22:05:23 -0300

    fix: fixes errors

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -2,12 +2,14 @@ import { UploadCsvButton } from '../UploadCsvButton/index.tsx';
 
 import { SearchBar } from '../SearchBar/index.tsx';
 
+import logo from '../../assets/images/logo.svg';
+
 export function Header() {
   return (
     <header className="fixed w-screen h-20 flex justify-center bg-zinc-800 shadow-md px-4 lg:px-0">
       <nav className="w-[996px] py-4 flex justify-between gap-4">
         <div className='flex items-center gap-4'>
-          <img src={'../../assets/images/logo.svg'} width={120} className="hidden lg:block"/>
+          <img src={logo} width={120} className="hidden lg:block"/>
 
           <div className="w-[1px] h-full bg-zinc-600 hidden lg:block"></div>
 
